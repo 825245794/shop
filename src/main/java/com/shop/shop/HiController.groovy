19b56458c83895a4a1a5417aaf5f668742360fc0
LJL <825245794@qq.com>
2018-11-06 23:06:14 +0800

    update

--- src/main/java/com/shop/shop/HiController.groovy
+++ src/main/java/com/shop/shop/HiController.groovy
@@ -1,14 +1,15 @@
 package com.shop.shop
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 class HiController {
-    @RequestMapping("/hi")
-    String index(){
-        return 'I am Groovy！！！'
+    @RequestMapping("/login")
+    String login(){
+        return 'login'
     }
 }
