d40473a6355436acafce26158dd68c81f8fd24f3
LJL <825245794@qq.com>
2018-11-07 23:26:51 +0800

    update

--- src/main/java/com/shop/shop/HiController.groovy
+++ src/main/java/com/shop/shop/HiController.groovy
@@ -5,11 +5,57 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.Security;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
 @Controller
 @EnableAutoConfiguration
 class HiController {
+    private static String src = "TestAES";
     @RequestMapping("/login")
     String login(){
         return 'login'
+    }
+    static void AES(){
+        //生成Key
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES")
+        keyGenerator.init(128);
+        //使用上面这种初始化方法可以特定种子来生成密钥，这样加密后的密文是唯一固定的。
+        SecretKey secretKey = keyGenerator.generateKey();
+        byte[] keyBytes = secretKey.getEncoded();
+
+        //Key转换
+        Key key = new SecretKeySpec(keyBytes, "AES");
+
+        //加密
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encodeResult = cipher.doFinal(src.getBytes());
+        System.out.println("AESencode : " + Hex.toHexString(encodeResult) );
+
+        //解密
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodeResult = cipher.doFinal(encodeResult);
+        System.out.println("AESdecode : " + new String (decodeResult));
+
+    }
+
+    static void main(String[] args){
+        AES()
     }
 }
